14ef86ce53b4a4cb692479e88be058ad3e90167d
infixsrf <hello.world.z@mail.ru>
2017-05-27 22:36:19 +0300

    Lesson 25.

---
+++ src/lesson25.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T, typename T2>
+
+void printArr (const T * array, int i) {
+	int count = 0;
+	for(int j = 0; j < i; i++) {
+		cout << array[j] << " ";
+		count++;
+	}
+	cout << endl;
+}
+
+int main () {
+
+	/*
+		В падлу что то писать.
+	*/
+
+
+	cin.get();
+	return 0;
+}
